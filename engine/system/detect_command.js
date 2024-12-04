1c1fd9747f634af34cc7fcc1a8142c64e777bc1a
Immortal Object <125799003+SenatorMorra@users.noreply.github.com>
2024-12-04 15:02:53 +0300

    v.1.0.0

---
+++ engine/system/detect_command.js
@@ -0,0 +1,14 @@
+export default function detect_command(line, engine, storage) {
+  switch (line[0]) {
+    case "":
+      return true;
+    case "log":
+      return engine.log(line, storage);
+    case "new":
+      return engine.new(line, storage);
+    case "set":
+      return engine.set(line, storage);
+    default:
+      return false;
+  }
+}
