8e1650bc880200356a71e41fec9f2a5f279c91e1
Immortal Object <125799003+SenatorMorra@users.noreply.github.com>
2024-12-05 15:32:00 +0300

    v.1.0.2
    requires storage

--- engine/command/delete.js
+++ engine/command/delete.js
@@ -1,5 +1,6 @@
-export default function delete_variable(line, storage) {
+export default function delete_variable(line, storage, is_start) {
   if (line[0] != "delete") return [false, "300"];
+  if (!is_start.status) return [false, "302"];
 
   let var_name = line[1];
   if (!storage.hasOwnProperty(var_name)) return [false, "301"];
