eba5901a9249b8d0441ee6cfa0e12b4aa768b727
Immortal Object <125799003+SenatorMorra@users.noreply.github.com>
2024-12-05 15:35:23 +0300

    v.1.0.2
    +start command

--- engine/system/detect_command.js
+++ engine/system/detect_command.js
@@ -1,16 +1,18 @@
-export default function detect_command(line, engine, storage) {
+export default function detect_command(line, engine, storage, is_start) {
   switch (line[0]) {
     case "":
       return true;
     case "log":
-      return engine.log(line, storage);
+      return engine.log(line, storage, is_start);
     case "new":
-      return engine.new(line, storage);
+      return engine.new(line, storage, is_start);
     case "set":
-      return engine.set(line, storage);
+      return engine.set(line, storage, is_start);
     case "delete":
-      return engine.delete(line, storage);
+      return engine.delete(line, storage, is_start);
+    case "start":
+      return engine.start(line, storage, is_start);
     default:
-      return false;
+      return [false, "0"];
   }
 }
