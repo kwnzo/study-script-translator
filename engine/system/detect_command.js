71619c61f95a11f9c81a2c64ab60b80fc7e8f929
Immortal Object <125799003+SenatorMorra@users.noreply.github.com>
2024-12-06 11:45:49 +0300

    v.1.0.3
    +if command
    +close command

--- engine/system/detect_command.js
+++ engine/system/detect_command.js
@@ -1,6 +1,22 @@
-export default function detect_command(line, engine, storage, is_start) {
+export default function detect_command(
+  line,
+  engine,
+  storage,
+  is_start,
+  ignore
+) {
+  if (line.length > 1) {
+    while (line[0] == "") {
+      line = line.slice(1);
+    }
+  }
+
+  if (ignore.status && ["close", "else"].indexOf(line[0]) == -1) return true;
+
   switch (line[0]) {
     case "":
+      return true;
+    case undefined:
       return true;
     case "log":
       return engine.log(line, storage, is_start);
@@ -12,6 +28,10 @@ export default function detect_command(line, engine, storage, is_start) {
       return engine.delete(line, storage, is_start);
     case "start":
       return engine.start(line, storage, is_start);
+    case "if":
+      return engine.if(line, storage, is_start, ignore);
+    case "close":
+      return engine.close(line, ignore);
     default:
       return [false, "0"];
   }
