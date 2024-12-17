144573fd5b3040f747ead24c578f5b049e2d8169
Immortal Object <125799003+SenatorMorra@users.noreply.github.com>
2024-12-17 10:35:00 +0300

    v.1.0.7

--- engine/system/garbage_collector.js
+++ engine/system/garbage_collector.js
@@ -2,12 +2,18 @@ export default function garbage_collector(storage) {
   let garbage = 0;
 
   for (let key in storage) {
-    if (storage[key].type == "string") garbage += storage[key].value.length * 4;
+    if (storage[key].type == "string")
+      garbage +=
+        storage[key].value.length *
+        4; // possible calculate the real number of bytes, not just * 4, there are many symbols for 1/2/3 bytes
     else if (storage[key].type == "number") {
       let number = Math.abs(storage[key].value);
-      if (number > 0) garbage += 1;
-      else if (number > 255) garbage += 1;
-      else if (number < 65536) garbage += 1;
+
+      if (number >= 0) garbage += 1;
+      if (number >= 256) garbage += 1;
+      else if (number >= 65536) garbage += 1;
+
+      if (storage[key].value < 0) garbage += 1;
     }
   }
 
