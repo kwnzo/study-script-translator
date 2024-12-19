3c770d304bcaa9cfa18be04eb1d32e7df3eaff37
Immortal Object <125799003+SenatorMorra@users.noreply.github.com>
2024-12-19 17:49:27 +0300

    v.1.0.9

--- engine/command/else.js
+++ engine/command/else.js
@@ -2,6 +2,11 @@ export default function else_sentence(line, ignore) {
   if (line[0] != "else") return [false, "700"];
   if (line.length > 1) return [false, "701"];
 
+  if (["all", "else"].indexOf(ignore.deep_ignore.at(-1)) != -1) {
+    ignore.status = true;
+    return true;
+  }
+
   if (ignore.status === true) ignore.status = false;
   else ignore.status = true;
 
