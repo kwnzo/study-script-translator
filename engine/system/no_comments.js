8f8b45c8eaa7f89488c9f14924d2275071544583
Immortal Object <125799003+SenatorMorra@users.noreply.github.com>
2024-12-20 11:28:06 +0300

    v.1.1.0

--- engine/system/no_comments.js
+++ engine/system/no_comments.js
@@ -1,12 +1,11 @@
 export default function no_comments(line) {
   if (line.indexOf("?") != -1)
-    line = line
-      .slice(0, line.indexOf("?"))
-      .split(" ")
-      .filter((x) => {
-        return x != "";
-      })
-      .join(" ");
+    line = line.slice(0, line.indexOf("?")).join(" ");
+
+  line = line
+    .split(" ")
+    .filter((x) => x != "")
+    .join(" ");
 
   return line;
 }
